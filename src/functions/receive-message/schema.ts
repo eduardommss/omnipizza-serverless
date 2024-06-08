cbdbe17ecc611b71a86ff4ee6ebf2926a4c1fb6c
Eduardo Moreira <eduardo_mmss@outlook.com>
2024-06-08 07:43:26 -0300

    feat: telegram integration added

--- src/functions/receive-message/schema.ts
+++ src/functions/receive-message/schema.ts
@@ -1,9 +1,42 @@
 // export default {
-//   type: "object",
-//   properties: {
-//     name: { type: 'string' }
+// 	type: "object",
+// 	properties: {
+//     name: { type: "string" },
 //   },
-//   required: ['name']
+// 	required: ["name"],
+// } as const;
+
+// export default {
+// 	type: "object",
+// 	properties: {
+// 		update_id: { type: "number" },
+// 		message: {
+// 			message_id: { type: "number" },
+// 			from: {
+// 				id: { type: "number" },
+// 				is_bot: { type: "boolean" },
+// 				first_name: { type: "string" },
+// 				last_name: { type: "string" },
+// 				username: { type: "string" },
+// 				language_code: { type: "string" },
+// 			},
+// 			chat: {
+// 				id: { type: "number" },
+// 				first_name: { type: "string" },
+// 				last_name: { type: "string" },
+// 				username: { type: "string" },
+// 				type: { type: "string" },
+// 			},
+// 			date: { type: "number" },
+// 			text: { type: "string" },
+// 			entities: {
+// 				offset: { type: "number" },
+// 				length: { type: "number" },
+// 				type: { type: "string" },
+// 			},
+// 		},
+// 	},
+// 	additionalProperties: true,
 // } as const;
 
 export default { type: "object", additionalProperties: true } as const;
